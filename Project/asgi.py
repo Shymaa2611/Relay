35ee0c11c95470f1738a81c5ab631defacacc5d4
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 01:58:00 +0300

    update

--- Project/asgi.py
+++ Project/asgi.py
@@ -6,27 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.layers import get_channel_layer
-from django.urls import path
-from api.consumers import NotificationConsumer
-from django.core.asgi import get_asgi_application
-from api import routing
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
-
-#application = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns  
-        )
-    ),
-})
+django.setup()
+application = get_default_application()
