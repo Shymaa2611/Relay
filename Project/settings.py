3584609088028b0c667633f95afc16a251629728
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-03 22:24:34 +0300

    update

--- Project/settings.py
+++ Project/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'django-insecure--sh^ej%h4v#es_xlnqq6#)=u2%j0upaeqm9r8#v!n7lra@gpk5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS=['*']
-#ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['Relay.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
