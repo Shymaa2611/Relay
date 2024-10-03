96c7a1c2468d0718fb0e4493ccf211ac39d309a1
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 00:43:27 +0300

    update

--- Project/settings.py
+++ Project/settings.py
@@ -26,8 +26,8 @@ from datetime import timedelta
 SECRET_KEY = 'django-insecure--sh^ej%h4v#es_xlnqq6#)=u2%j0upaeqm9r8#v!n7lra@gpk5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS=['*']
+DEBUG = False
+#ALLOWED_HOSTS=['*']
 ALLOWED_HOSTS = ['relayapi-7776c82ed8cf.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
