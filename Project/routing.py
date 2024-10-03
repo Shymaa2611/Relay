399f81829db0d628ded48e7f0cc893ec35719012
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 02:10:26 +0300

    update

--- Project/routing.py
+++ Project/routing.py
@@ -1,8 +1,9 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import api.routing
-
+from django.core.asgi import get_asgi_application
 application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             api.routing.websocket_urlpatterns
