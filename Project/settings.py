0f3219cc1e1f9db6e2a0d17b7a423c3be63a625b
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-03 23:59:18 +0300

    update

--- Project/settings.py
+++ Project/settings.py
@@ -33,11 +33,11 @@ ALLOWED_HOSTS = ['relayapi-7776c82ed8cf.herokuapp.com', 'localhost', '127.0.0.1'
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     "whitenoise.runserver_nostatic",
     'api',
     'notifications',
     'accounts',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -126,14 +126,32 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
-DATABASES = {
+# SECURITY WARNING: don't run with debug turned on in production!
+if IS_HEROKU_APP:
+    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
+    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
+    # automatically by Heroku when a database addon is attached to your Heroku app. See:
+    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
+    # https://github.com/jazzband/dj-database-url
+    DATABASES = {
+        "default": dj_database_url.config(
+            env="DATABASE_URL",
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
+    }
+else:
+    # When running locally in development or in CI, a sqlite database file will be used instead
+    # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
+    DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
 
 
 
