c76c5b3e2ecb3a95373a4478d14b96cbdd187adc
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 03:14:34 +0300

    update

--- api/consumers.py
+++ api/consumers.py
@@ -14,7 +14,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         logger.info(f"Client connected: {self}")
 
     async def disconnect(self, close_code):
-        await NotificationConsumer.connected_clients.remove(self)
+        NotificationConsumer.connected_clients.remove(self)
         logger.info(f"Client disconnected: {self}")
 
     async def receive(self, text_data=None):
