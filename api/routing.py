03da88b9cca0eb5a46284b7edcf4b01c0675fc2d
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 20:39:07 +0300

    update

--- api/routing.py
+++ api/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
-from .consumers import TestConsumer
+from .consumers import NotificationConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/notifications/$', TestConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
     
 ]
  
