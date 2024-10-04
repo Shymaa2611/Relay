aecc480b6880b9a84e855d77a1bc6eced530a8ba
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-10-04 21:10:37 +0300

    update

--- Project/asgi.py
+++ Project/asgi.py
@@ -1,13 +1,26 @@
+"""
+ASGI config for Project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from api.consumers import NotificationConsumer
 from django.core.asgi import get_asgi_application
+from api import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/notifications/", NotificationConsumer.as_asgi()),
-        ])
+        URLRouter(
+            routing.websocket_urlpatterns  
+        )
     ),
 })
