222e6d2b54c6b4a58e634496644d25fd846eb43b
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-11-08 01:44:00 +0200

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from api.views import send_notification
+#from api.views import send_notification
 
 urlpatterns = [
-    path("send-notification/", send_notification, name="send_notification")
+    #path("send-notification/", send_notification, name="send_notification")
  
 ]
